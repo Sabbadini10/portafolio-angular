9dea17043799c71cb4d6da7955a5569429196711
Sabbadini10 <matiassabbadini24@gmail.com>
2023-03-12 19:40:28 -0300

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/component/navbar/navbar.component';
+import { SharedModule } from './shared/component/shared.module';
+import { SobreMiComponent } from './sobreMi/sobre-mi/sobre-mi.component';
+import { SkillsComponent } from './skills/skills/skills.component';
+import { ProyectosComponent } from './proyectos/proyectos/proyectos.component';
+import { CursosComponent } from './cursos/cursos/cursos.component';
+import { ContactoComponent } from './contacto/contacto/contacto.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreMiComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    CursosComponent,
+    ContactoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
